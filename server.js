82f697edfc9c77ccd4789d5f5d36418f684ceb2d
sudeeprss83 <sudeep.rss83@gmail.com>
2020-07-01 23:56:46 +0530

    file upload and preview completed

--- server.js
+++ server.js
@@ -1,12 +1,54 @@
 const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
 
 //creating express app
 const app = express();
 app.use(express.json());
 
+let filepath;
+
+//multer storage configuration
+const storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, "./uploads");
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.fieldname + "_" + Date.now());
+  },
+});
+
+const upload = multer({ storage }).single("fileUpload");
+
+app.use(express.static("public"));
+
 //Fetching the home page
-app.get("/", (req, res) => {
-  res.send("home");
+app.post("/upload", (req, res) => {
+  upload(req, res, (err) => {
+    if (err) {
+      return res.end("Error uploading file.");
+    }
+    res.sendFile(__dirname + "/public/preview.html");
+  });
+});
+
+app.get("/upload/preview", (req, res) => {
+  images = [];
+
+  fs.readdir(`${__dirname}/uploads`, (err, files) => {
+    if (err) {
+      throw err;
+    }
+    files.forEach((file) => {
+      images.push(file);
+    });
+    const filepath = path.join(__dirname, `/uploads/${images[0]}`);
+    const readstream = fs.createReadStream(filepath);
+    readstream.on("open", () => {
+      readstream.pipe(res);
+    });
+  });
 });
 
 //connecting to the server port
